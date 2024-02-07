d21828cacf29bb51c45336076dbcb8b2ef846e3e
Aber-Racheal <rachealaberr@gmail.com>
2024-02-07 22:55:00 +0300

    Kotlin_Project

---
+++ aberRacheal/src/main/kotlin/Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var name = "aberRacheal"
+    println(name)
+    var age: Byte = 24
+    println(age)
+    var phoneNumber = "07786543289"
+    println(phoneNumber)
+    var weightInKg: Float = 81.2F
+    println(weightInKg)
+    var kenyan: Boolean = true or false
+    println(kenyan)
+    var nonKenyan: Boolean = false
+    println(nonKenyan)
+
+
+
+}
